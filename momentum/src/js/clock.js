5ff0f647a7fe50659a62c1bdeb96a80e55afdc1f
BlackBerryID <iddf1994one@gmail.com>
2021-10-15 20:52:15 +0300

    fear: add clock

---
+++ momentum/src/js/clock.js
@@ -0,0 +1,19 @@
+const timePlace = document.querySelector(".time");
+const datePlace = document.querySelector(".date");
+
+function showDate() {
+  const date = new Date();
+  const options = { weekday: "long", month: "long", day: "numeric" };
+  const currentDate = date.toLocaleDateString("en-US", options);
+  datePlace.textContent = currentDate;
+}
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  timePlace.textContent = currentTime;
+  showDate();
+  setTimeout(showTime, 1000);
+}
+
+showTime();
