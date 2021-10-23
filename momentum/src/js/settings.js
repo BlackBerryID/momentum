6318c7ce4cb74ea72443b5345ead40ae7a6a9d46
BlackBerryID <iddf1994one@gmail.com>
2021-10-23 13:52:23 +0300

    feat: link show settings to localStorage

--- momentum/src/js/settings.js
+++ momentum/src/js/settings.js
@@ -1,5 +1,29 @@
 const settings = document.querySelector(".settings");
 const openSettingsButton = document.querySelector(".open-settings");
+const settingCheckboxes = document.querySelectorAll(".checkbox");
+let state = {
+  language: "en",
+  photoSource: "github",
+  blocks: ["time", "date", "greeting", "quote", "weather", "audio", "todolist"],
+};
+
+if (!localStorage.getItem("state")) {
+  localStorage.setItem("state", JSON.stringify(state));
+} else {
+  state = JSON.parse(localStorage.getItem("state"));
+}
+
+settingCheckboxes.forEach((item) => {
+  for (let i = 0; i < state.blocks.length; i++) {
+    if (item.dataset.name === `${state.blocks[i]}`) {
+      item.checked = true;
+    }
+  }
+});
+
+function setLocalStorage() {
+  localStorage.setItem("state", JSON.stringify(state));
+}
 
 function toggleSettings() {
   if (settings.classList.contains("active")) {
@@ -9,7 +33,22 @@ function toggleSettings() {
   }
 }
 
+function changeBlocksState() {
+  if (this.checked) {
+    if (!state.blocks.includes(this.dataset.name)) {
+      state.blocks.push(this.dataset.name);
+    }
+  } else {
+    let index = state.blocks.findIndex((item) => item === this.dataset.name);
+    state.blocks.splice(index, 1);
+  }
+}
+
+window.addEventListener("beforeunload", setLocalStorage);
 openSettingsButton.addEventListener("click", toggleSettings);
+settingCheckboxes.forEach((item) =>
+  item.addEventListener("change", changeBlocksState)
+);
 
 // switch
 const switchWrapper = document.querySelector(".switch-wrapper");
